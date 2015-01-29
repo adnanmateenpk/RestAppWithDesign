b4650da93a3302c005a2734f6102d50e972492f9
Adnan <adnan@ubuntu.ubuntu-domain>
2015-01-30 02:42:52 +0500

    create Read me

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,21 @@ class PagesController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+  def create  	
+  end
+
   def edit
   end
 
+  def update
+  end
+
   def delete
+  end
+
+  def destroy
   end
 end
