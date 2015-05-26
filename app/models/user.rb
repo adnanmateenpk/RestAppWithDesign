627bd72887b87eac7c6489a3311fb9df1d9d0f17
Adnan Mateen <adnanmateen@outlook.com>
2015-05-26 20:26:38 +0500

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,8 @@ class User < ActiveRecord::Base
   #validations
   validates :name,		:presence => true,
 							:length => { :maximum => 25 }
-  validates :phone,    :presence => true  ,:numericality => { :only_integer => true , :greater_than => 0 }
-  validates :membership,:uniqueness => true, :format => {:with => "^[0-9]*$" , :message => "Not a Valid Number"}
+  validates :phone,    :presence => true  ,:format => {:with => "^[0-9]*$" , :message => "Not a Valid Number"}
+  validates :membership,:uniqueness => true, 
                         :length => { :maximum => 6 },
                         :presence => true
 end
