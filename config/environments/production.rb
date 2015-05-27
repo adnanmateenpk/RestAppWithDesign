07c818aca01d3626169153fb4b76715cc78a0ffa
Adnan Mateen <adnanmateen@outlook.com>
2015-05-27 20:08:19 +0500

    upload test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_host_name => "reservados.co.s3-website-us-east-1.amazonaws.com",
+    :s3_host_name => "s3-us-east-1.amazonaws.com",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
